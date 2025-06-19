e0db4ce780f31354db894cd094b813c49e846517
vibek01 <vibekbin305@gmail.com>
2025-06-19 22:15:23 +0530

    added .env and configured vite.config.js for cloudflare deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import solid from 'vite-plugin-solid'
 
-export default defineConfig({
-  plugins: [solid()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': 'http://localhost:8000',
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+
+  return defineConfig({
+    plugins: [solid()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL.replace(/\/api$/, ''),
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, '')
+        }
+      }
     },
-  },
-});
+    define: {
+      'process.env': {}
+    }
+  })
+}
